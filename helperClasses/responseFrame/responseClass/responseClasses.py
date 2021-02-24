5e9cd460dcba2eb524481532f489bcbac5e90cc3
vaibhav <vaibhav0195@gmail.com>
2021-02-24 15:18:22 +0000

    missing files commit

--- helperClasses/responseFrame/responseClass/responseClasses.py
+++ helperClasses/responseFrame/responseClass/responseClasses.py
@@ -1,22 +1,22 @@
-from helperClasses.responseFrame.responseClass.responseBase import Response
+from helperClasses.responseFrame.responseClass.vehicle import Vehicle
 
-class PoliceCar(Response,):
+class PoliceCar(Vehicle,):
     def __init__(self,name,location):
-        super().__init__(name,location)
+        super().__init__(name,"Policecar",location)
 
     def actionAtSite(self):
         return 'block'
 
-class Firetruck(Response,):
+class Firetruck(Vehicle,):
     def __init__(self,name,location):
-        super().__init__(name,location)
+        super().__init__(name,"FireTruck",location)
 
     def actionAtSite(self):
         return 'Reduce Fire'
 
-class Ambulance(Response,):
+class Ambulance(Vehicle,):
     def __init__(self,name,location):
-        super().__init__(name,location)
+        super().__init__(name,"Ambulance",location)
 
     def actionAtSite(self):
         return 'Pick and go'
